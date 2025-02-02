99f51160f0f4f9831cb9c938a9e3eef983867731
zibby-zibs <oloyedetimi@gmail.com>
2025-02-02 21:22:21 +0100

    fix: refactor payment verification logic and enhance activity logging

--- src/app/(landingPage)/content-library/_components/verify-payment-modal.tsx
+++ src/app/(landingPage)/content-library/_components/verify-payment-modal.tsx
@@ -9,6 +9,7 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+import { PaystackVerify } from "@/constants/global";
 import { useQuery } from "@tanstack/react-query";
 import { Loader2 } from "lucide-react";
 import { useRouter, useSearchParams } from "next/navigation";
@@ -26,42 +27,54 @@ const VerifyPayment = ({ userId }: Props) => {
   const params = new URLSearchParams(searchParams.toString());
   const { data, isPending, isError, isSuccess } = useQuery({
     queryKey: ["verify-payment"],
-    queryFn: () => verifyPaystackPayment(reference!),
+    queryFn: () => {
+      const verify = () => {
+        verifyPaystackPayment(reference!).then((data) => {
+          if (data) {
+            addActivity(data);
+          } else {
+            toast.error("An error occurred while verifying payment");
+          }
+        });
+      };
+
+      verify();
+    },
     enabled: !!reference,
   });
 
-  const onOpenChange = () => {
-    if (data?.status) {
-      params.delete("reference");
-      router.replace("/home");
+  // const onOpenChange = () => {
+  //   if (data?.status) {
+  //     params.delete("reference");
+  //     router.replace("/home");
+  //   }
+  // };
+  async function addActivity(data: PaystackVerify) {
+    try {
+      const activity = await addToActivityLog(
+        userId,
+        `Paid for ${data?.data?.metadata?.serviceType}`,
+        data?.data?.metadata.serviceType,
+        data?.data?.amount / 100,
+        data?.data?.reference
+      );
+
+      toast.success("Payment confirmed successfully");
+      if (activity.status === 200) {
+        router.replace(`/home`);
+      }
+    } catch (error) {
+      toast.error("An error occurred while adding activity log");
+      console.log("ADD_ACTIVITY_ERROR", error);
     }
-  };
+  }
 
-  useEffect(() => {
-    if (data?.status && isSuccess) {
-      const addActivity = async () => {
-        try {
-          const activity = await addToActivityLog(
-            userId,
-            `Paid for ${data?.data?.metadata?.serviceType}`,
-            data?.data?.metadata.serviceType,
-            data?.data?.amount / 100,
-            data?.data?.reference
-          );
+  // useEffect(() => {
+  //   if (data?.status && isSuccess) {
 
-          toast.success("Payment confirmed successfully");
-          if (activity.status === 200) {
-            router.replace(`/home`);
-          }
-        } catch (error) {
-          toast.error("An error occurred while adding activity log");
-          console.log("ADD_ACTIVITY_ERROR", error);
-        }
-      };
-
-      addActivity();
-    }
-  }, [data?.status, isSuccess]);
+  //     addActivity();
+  //   }
+  // }, [data?.status, isSuccess]);
 
   // useEffect(() => {
   //   console.log(data, isSuccess);
@@ -83,13 +96,9 @@ const VerifyPayment = ({ userId }: Props) => {
             <div className="flex justify-center items-center">
               <Loader2 className="text-blue-500 w-10 h-10 animate-spin" />
             </div>
-          ) : data?.status ? (
+          ) : (
             <h1 className="text-3xl text-center font-medium font-poppins">
               Payment Verified
-            </h1>
-          ) : (
-            <h1 className="text-3xl text-center font-medium font-poppins text-red-500">
-              Payment Verification Failed - {data?.message}
             </h1>
           )}
         </div>
